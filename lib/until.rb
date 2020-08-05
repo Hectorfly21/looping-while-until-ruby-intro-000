092d2465d737ec69087fbbdee4abf50e241e0b37
Hector <hectorfly21@gmail.com>
2020-08-05 03:16:41 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
-  until levitation_force == 10 #your code here
-    puts "Wingardium Leviosa is equal to 10."
+  until levitation_force == 11 #your code here
+    puts "Wingardium Leviosa is equal to 11 "
     levitation_force += 1
   end
 end
